84099c88137f11cfc3a5501b1a2c0fbc1ac96679
petterqiu <166462268+petterqiu@users.noreply.github.com>
2024-07-14 22:35:39 +0900

    Update homework12.py

--- 2024_spring/lec12/homework12.py
+++ 2024_spring/lec12/homework12.py
@@ -1,13 +1,14 @@
-import gtts
+from gtts import gTTS
 
 def synthesize(text, lang, filename):
     '''
-    Use gtts.gTTs(text=text, lang=lang) to synthesize speech, then write it to filename.
+    Use gTTS (Google Text-to-Speech) to synthesize speech, then save it to a file.
     
     @params:
-    text (str) - the text you want to synthesize
-    lang (str) - the language in which you want to synthesize it
-    filename (str) - the filename in which it should be saved
+    text (str) - the text to synthesize
+    lang (str) - the language in which to synthesize the text
+    filename (str) - the filename where the synthesized speech should be saved
     '''
-    raise RuntimeError("FAIL! You need to change this function so that it works!")
+    tts = gTTS(text=text, lang=lang)
+    tts.save(filename)
 
